1a032531ea3e2835a0169215e030f097507fca0b
bestradag05 <bestradag05@gmail.com>
2023-11-22 17:04:42 -0500

    Register and show patients

--- frontend/src/components/Formulario.jsx
+++ frontend/src/components/Formulario.jsx
@@ -16,27 +16,29 @@ const Formulario = () => {
 
 
     const handleSubmit = e => {
-            e.preventDefault();
+        e.preventDefault();
 
-            // Validar el formulario
+        // Validar el formulario
 
-            if([nombre, propietario, email, fecha, sintomas].includes('')){
+        if ([nombre, propietario, email, fecha, sintomas].includes('')) {
 
-                setAlerta({
-                    msg: 'Todos los campos son obligatorios',
-                    error: true,
-                })
-            }
-
-            setAlerta({});
-
-            guardarPaciente({
-                nombre, propietario, email, fecha, sintomas
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true,
             })
+        }
+
+        setAlerta({});
+
+        guardarPaciente({
+            nombre, propietario, email, fecha, sintomas
+        })
     }
 
     return (
         <>
+
+            <h2 className="font-black text-3xl text-center">Administrador de pacientes </h2>
             <p className="text-lg text-center mb-10">
                 Añade tus pacientes y
                 <span className="text-indigo-600 font-bold"> Administralos</span>
@@ -45,86 +47,86 @@ const Formulario = () => {
             {alerta.msg && <Alerta alerta={alerta} />}
 
             <form action="" className="bg-white py-10 px-5 mb-10 lg:mb-0 shadow-md rounded-md"
-            onSubmit={handleSubmit}>
+                onSubmit={handleSubmit}>
                 <div className="mb-5">
                     <label htmlFor="nombre"
-                    className="text-gray-700 uppercase font-bold"
+                        className="text-gray-700 uppercase font-bold"
                     >
                         Nombre Mascota
                     </label>
-                    <input 
-                    type="text"
-                    id="mascota"
-                    placeholder="Nombre de la mascota"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    value={nombre}
-                    onChange={e => setNombre(e.target.value)}/>
+                    <input
+                        type="text"
+                        id="mascota"
+                        placeholder="Nombre de la mascota"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="propietario"
-                    className="text-gray-700 uppercase font-bold"
+                        className="text-gray-700 uppercase font-bold"
                     >
                         Nombre Propietario
                     </label>
-                    <input 
-                    type="text"
-                    id="propietario"
-                    placeholder="Nombre del propietario"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    value={propietario}
-                    onChange={e => setPropietario(e.target.value)}/>
+                    <input
+                        type="text"
+                        id="propietario"
+                        placeholder="Nombre del propietario"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                        onChange={e => setPropietario(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="email"
-                    className="text-gray-700 uppercase font-bold"
+                        className="text-gray-700 uppercase font-bold"
                     >
                         Email Propietario
                     </label>
-                    <input 
-                    type="email"
-                    id="email"
-                    placeholder="Nombre del email"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="Nombre del email"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="fecha"
-                    className="text-gray-700 uppercase font-bold"
+                        className="text-gray-700 uppercase font-bold"
                     >
                         Fecha alta
                     </label>
-                    <input 
-                    type="date"
-                    id="fecha"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    value={fecha}
-                    onChange={e => setFecha(e.target.value)}/>
+                    <input
+                        type="date"
+                        id="fecha"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                        onChange={e => setFecha(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="sintomas"
-                    className="text-gray-700 uppercase font-bold"
+                        className="text-gray-700 uppercase font-bold"
                     >
                         Sintomas
                     </label>
                     <textarea
-                    id="fecha"
-                    placeholder="Describe los sintomas"
-                    className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    value={sintomas}
-                    onChange={e => setSintomas(e.target.value)}
+                        id="fecha"
+                        placeholder="Describe los sintomas"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={sintomas}
+                        onChange={e => setSintomas(e.target.value)}
                     ></textarea>
                 </div>
 
-                <input 
-                type="submit"  
-                value="Agregar Paciente"
-                className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                <input
+                    type="submit"
+                    value="Agregar Paciente"
+                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
 
                 />
             </form>
--- frontend/src/context/PacientesProvides.jsx
+++ frontend/src/context/PacientesProvides.jsx
@@ -9,6 +9,32 @@ export  const PacientesProvider =  ({children}) =>{
 
     const [pacientes, setPacientes] = useState([]);
 
+
+    useEffect(() => {
+        const obtenerPacientes = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                if(!token) return
+
+                const config = {
+                    headers: {
+                        "Content-Type" : "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+
+                const { data } = await clienteAxios('/pacientes', config);
+                setPacientes(data);
+            } catch (error) {
+
+                console.log(error);
+                
+            }
+        }
+
+        obtenerPacientes();
+    }, [])
+
     const guardarPaciente = async (paciente) => {
         try {
             const token = localStorage.getItem('token');
