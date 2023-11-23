ca23cb629eb9395abd4e7b29d58da126b99e8a11
bestradag05 <bestradag05@gmail.com>
2023-11-22 22:10:29 -0500

    crud completed

--- frontend/src/components/Paciente.jsx
+++ frontend/src/components/Paciente.jsx
@@ -1,9 +1,11 @@
-
-const Paciente = (paciente) => {
-
+import usePacientes from "../../hooks/usePacientes";
 
 
-    const { email, fecha, nombre, propietario, sintomas, _id } = paciente.paciente;
+const Paciente = ({paciente}) => {
+
+    const { setEdicion, eliminarPaciente } = usePacientes();
+
+    const { email, fecha, nombre, propietario, sintomas, _id } = paciente;
 
 
     const formatearFecha = (fecha) => {
@@ -11,6 +13,7 @@ const Paciente = (paciente) => {
 
         return new Intl.DateTimeFormat('es-PE', {dateStyle: 'long'}).format(nuevaFecha);
     }
+
 
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
@@ -31,9 +34,13 @@ const Paciente = (paciente) => {
         </p>
 
         <div className="flex justify-between my-5">
-            <button type="button" className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg">Editar</button>
+            <button type="button" className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+            onClick={() => setEdicion(paciente)}
+            >Editar</button>
 
-            <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg">Eliminar</button>
+            <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg"
+            onClick={() => eliminarPaciente(_id)}
+            >Eliminar</button>
         </div>
 
     </div>
